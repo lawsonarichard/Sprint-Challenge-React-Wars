5479978cb860867f5f692b949d407e45593827bb
Richard Lawson <lawson.a.richard@gmail.com>
2019-07-19 13:38:22 -0700

    forgot to map have mapped and redone

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import CardBodyExample from './components/cards/cards';
-
+import Cards from './components/cards/cards';
+import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
 
@@ -12,11 +12,22 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+        
+        useEffect(() => {
+        axios
+        .get(`https://swapi.co/api/people`)
+        .then( res => {
+         console.log("I'm working", res);
+            setCharacters(res.data.results);
+        });
+        }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <CardBodyExample />
+      <Cards characters={characters}/>
     </div>
   );
 }
--- starwars/src/components/cards/cards.js
+++ starwars/src/components/cards/cards.js
@@ -1,62 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container, Row, Col, Card, CardBody, CardImg } from "shards-react";
-import axios from 'axios';
 
-
-export default function CardBodyExample() {
-        const [name, setName] = useState([]);
-        const [height, setHeight] = useState([]);
-        const [mass, setMass] = useState([]);
-        const [haircolor, setHaircolor] = useState([]);
-        const [eyecolor, setEyecolor] = useState([]);
-        const [birthyear, setBirthyear] = useState([]);
-        const [gender, setGender] = useState([]);
-        useEffect(() => {
-        axios
-        .get(`https://swapi.co/api/people/4`)
-        .then( res => {
-            console.log(res.data);
-            const name = res.data.name;
-            setName(name);
-            const height = res.data.height;
-            setHeight(height);
-            const mass = res.data.mass;
-            setMass(mass);
-            const haircolor = res.data.hair_color;
-            setHaircolor(haircolor);
-            const eyecolor = res.data.eye_color;
-            setEyecolor(eyecolor);
-            const birthyear = res.data.birth_year;
-            setBirthyear(birthyear);
-            const gender = res.data.gender;
-            setGender(gender);
-
-
-        });
-        }, []);
-
-
-  return (
-    <Container>
-      <Row>
+const Cards = ({ characters }) => (
+       
+     <div className="cardWrapper">
+                            {characters.map(character => {
+        return  (
+            <Container> 
+    <Row>
         <Col>
           <Card>
-            <CardImg top src="https://www.lego.com/r/www/r/catalogs/-/media/catalogs/characters/star%20wars/new%20thumbnails/darth_vader-thumb.png" />
+            
             <CardBody>
-              <p>{name}</p>
-              <p>Gender: {gender}</p>
-              <p>Height: {height}</p>
-              <p>Mass: {mass}</p>
-              <p>Hair Color: {haircolor}</p>
-              <p>Eye Color: {eyecolor}</p>
-              <p>Birth Year: {birthyear}</p>
-
+    
+            <p>{character.name}</p>
+            <p>Height: {character.height}</p>
+            <p>Mass: {character.mass}</p>
+            <p>Hair-Color: {character.hair_color}</p>
+            <p>DOB: {character.birth_year}</p>
             </CardBody>
-          </Card>
-        </Col>
-
+            </Card>
+            </Col>
+            </Row>  
+            </Container>
+        )
+       
         
-      </Row>
-    </Container>
-  );
-}
+       
+
+
+        })}
+         </div>
+        
+    )
+
+
+
+
+
+    
+export default Cards;
